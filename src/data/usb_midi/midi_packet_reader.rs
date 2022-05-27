1e16171e7bb13e42d253ce694e618cd3cf26e70f
Alex Norman <alex@x37v.info>
2022-05-27 08:41:59 -0700

    cargo format

--- src/data/usb_midi/midi_packet_reader.rs
+++ src/data/usb_midi/midi_packet_reader.rs
@@ -1,11 +1,11 @@
-use core::convert::TryFrom;
-use crate::data::usb_midi::usb_midi_event_packet::{UsbMidiEventPacket, MidiPacketParsingError};
+use crate::data::usb_midi::usb_midi_event_packet::{MidiPacketParsingError, UsbMidiEventPacket};
 use crate::midi_device::{MAX_PACKET_SIZE, MIDI_PACKET_SIZE};
+use core::convert::TryFrom;
 
 pub struct MidiPacketBufferReader<'a> {
     buffer: &'a [u8; MAX_PACKET_SIZE],
     position: usize,
-    raw_bytes_received: usize
+    raw_bytes_received: usize,
 }
 
 impl<'a> MidiPacketBufferReader<'a> {
@@ -13,7 +13,7 @@ impl<'a> MidiPacketBufferReader<'a> {
         MidiPacketBufferReader {
             buffer,
             position: 0,
-            raw_bytes_received
+            raw_bytes_received,
         }
     }
 }
@@ -23,9 +23,12 @@ impl<'a> Iterator for MidiPacketBufferReader<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.position <= MAX_PACKET_SIZE && self.position < self.raw_bytes_received {
-            let packet = match self.buffer.get(self.position .. (self.position + MIDI_PACKET_SIZE)) {
+            let packet = match self
+                .buffer
+                .get(self.position..(self.position + MIDI_PACKET_SIZE))
+            {
                 Some(packet) => Some(UsbMidiEventPacket::try_from(packet)),
-                None => None
+                None => None,
             };
 
             self.position += MIDI_PACKET_SIZE;
--- src/midi_device.rs
+++ src/midi_device.rs
@@ -1,7 +1,7 @@
+use crate::data::usb::constants::*;
+use crate::data::usb_midi::usb_midi_event_packet::{MidiPacketParsingError, UsbMidiEventPacket};
 use usb_device::class_prelude::*;
 use usb_device::Result;
-use crate::data::usb::constants::*;
-use crate::data::usb_midi::usb_midi_event_packet::{UsbMidiEventPacket, MidiPacketParsingError};
 
 const MIDI_IN_SIZE: u8 = 0x06;
 const MIDI_OUT_SIZE: u8 = 0x09;
@@ -12,18 +12,18 @@ pub const MAX_PACKET_SIZE: usize = 64;
 ///Note we are using MidiIn here to refer to the fact that
 ///The Host sees it as a midi in device
 ///This class allows you to send data in
-pub struct MidiClass<'a,B: UsbBus> {
+pub struct MidiClass<'a, B: UsbBus> {
     standard_ac: InterfaceNumber,
     standard_mc: InterfaceNumber,
     standard_bulkout: EndpointOut<'a, B>,
-    standard_bulkin: EndpointIn<'a,B>,
+    standard_bulkin: EndpointIn<'a, B>,
     n_in_jacks: u8,
     n_out_jacks: u8,
 }
 
 pub enum MidiReadError {
     ParsingFailed(MidiPacketParsingError),
-    UsbError(UsbError)
+    UsbError(UsbError),
 }
 
 #[derive(Debug)]
@@ -33,17 +33,21 @@ impl<B: UsbBus> MidiClass<'_, B> {
     /// Creates a new MidiClass with the provided UsbBus and `n_in/out_jacks` embedded input/output jacks (or "cables",
     /// depending on the terminology).
     /// Note that a maximum of 16 in and 16 out jacks are supported.
-    pub fn new(alloc: &UsbBusAllocator<B>, n_in_jacks: u8, n_out_jacks: u8) -> core::result::Result<MidiClass<'_, B>, InvalidArguments>  {
+    pub fn new(
+        alloc: &UsbBusAllocator<B>,
+        n_in_jacks: u8,
+        n_out_jacks: u8,
+    ) -> core::result::Result<MidiClass<'_, B>, InvalidArguments> {
         if n_in_jacks > 16 || n_out_jacks > 16 {
             return Err(InvalidArguments);
         }
         Ok(MidiClass {
             standard_ac: alloc.interface(),
             standard_mc: alloc.interface(),
-            standard_bulkout : alloc.bulk(MAX_PACKET_SIZE as u16),
+            standard_bulkout: alloc.bulk(MAX_PACKET_SIZE as u16),
             standard_bulkin: alloc.bulk(MAX_PACKET_SIZE as u16),
             n_in_jacks,
-            n_out_jacks
+            n_out_jacks,
         })
     }
 
@@ -51,7 +55,7 @@ impl<B: UsbBus> MidiClass<'_, B> {
         self.standard_bulkin.write(&buffer)
     }
     pub fn send_message(&mut self, usb_midi: UsbMidiEventPacket) -> Result<usize> {
-        let bytes : [u8;MIDI_PACKET_SIZE] = usb_midi.into();
+        let bytes: [u8; MIDI_PACKET_SIZE] = usb_midi.into();
         self.standard_bulkin.write(&bytes)
     }
 
@@ -83,14 +87,13 @@ impl<B: UsbBus> MidiClass<'_, B> {
 }
 
 impl<B: UsbBus> UsbClass<B> for MidiClass<'_, B> {
-
-     fn get_configuration_descriptors(&self, writer: &mut DescriptorWriter) -> Result<()> {
+    fn get_configuration_descriptors(&self, writer: &mut DescriptorWriter) -> Result<()> {
         //AUDIO CONTROL STANDARD
         writer.interface(
             self.standard_ac,
             USB_AUDIO_CLASS,
             USB_AUDIOCONTROL_SUBCLASS,
-            0 //no protocol,
+            0, //no protocol,
         )?;
 
         // AUDIO CONTROL EXTRA INFO
@@ -98,11 +101,13 @@ impl<B: UsbBus> UsbClass<B> for MidiClass<'_, B> {
             CS_INTERFACE,
             &[
                 HEADER_SUBTYPE,
-                0x00,0x01, // REVISION
-                0x09,0x00, //SIZE of class specific descriptions
+                0x00,
+                0x01, // REVISION
+                0x09,
+                0x00, //SIZE of class specific descriptions
                 0x01, //Number of streaming interfaces
-                0x01 // MIDIStreaming interface 1 belongs to this AC interface
-            ]
+                0x01, // MIDIStreaming interface 1 belongs to this AC interface
+            ],
         )?;
 
         //Streaming Standard
@@ -115,79 +120,90 @@ impl<B: UsbBus> UsbClass<B> for MidiClass<'_, B> {
         )?; //Num endpoints?
 
         let midi_streaming_start_byte = writer.position();
-        let midi_streaming_total_length =
-            7 + (self.n_in_jacks + self.n_out_jacks) as usize * (MIDI_IN_SIZE + MIDI_OUT_SIZE) as usize
-            + 7 + (4+self.n_out_jacks as usize) + 7 + (4+self.n_in_jacks as usize);
+        let midi_streaming_total_length = 7
+            + (self.n_in_jacks + self.n_out_jacks) as usize
+                * (MIDI_IN_SIZE + MIDI_OUT_SIZE) as usize
+            + 7
+            + (4 + self.n_out_jacks as usize)
+            + 7
+            + (4 + self.n_in_jacks as usize);
 
         //Streaming extra info
-        writer.write( // len = 7
+        writer.write(
+            // len = 7
             CS_INTERFACE,
             &[
                 MS_HEADER_SUBTYPE,
-                0x00,0x01, //REVISION
-                (midi_streaming_total_length & 0xFF) as u8, ((midi_streaming_total_length >> 8) & 0xFF) as u8
-            ]
+                0x00,
+                0x01, //REVISION
+                (midi_streaming_total_length & 0xFF) as u8,
+                ((midi_streaming_total_length >> 8) & 0xFF) as u8,
+            ],
         )?;
 
         //JACKS
         for i in 0..self.n_in_jacks {
-            writer.write( // len = 6 = MIDI_IN_SIZE
+            writer.write(
+                // len = 6 = MIDI_IN_SIZE
                 CS_INTERFACE,
                 &[
                     MIDI_IN_JACK_SUBTYPE,
                     EXTERNAL,
                     self.in_jack_id_ext(i), // id
-                    0x00
-                ]
+                    0x00,
+                ],
             )?;
         }
 
         for i in 0..self.n_out_jacks {
-            writer.write( // len = 6 = MIDI_IN_SIZE
+            writer.write(
+                // len = 6 = MIDI_IN_SIZE
                 CS_INTERFACE,
                 &[
                     MIDI_IN_JACK_SUBTYPE,
                     EMBEDDED,
                     self.in_jack_id_emb(i), // id
-                    0x00
-                ]
+                    0x00,
+                ],
             )?;
         }
 
         for i in 0..self.n_out_jacks {
-            writer.write ( // len = 9 = MIDI_OUT_SIZE
+            writer.write(
+                // len = 9 = MIDI_OUT_SIZE
                 CS_INTERFACE,
                 &[
                     MIDI_OUT_JACK_SUBTYPE,
                     EXTERNAL,
                     self.out_jack_id_ext(i), //id
-                    0x01, // 1 pin
-                    self.in_jack_id_emb(i), // pin is connected to this entity...
-                    0x01, // ...to the first pin
-                    0x00
-                ]
+                    0x01,                    // 1 pin
+                    self.in_jack_id_emb(i),  // pin is connected to this entity...
+                    0x01,                    // ...to the first pin
+                    0x00,
+                ],
             )?;
         }
 
         for i in 0..self.n_in_jacks {
-            writer.write ( // len = 9 = MIDI_OUT_SIZE
+            writer.write(
+                // len = 9 = MIDI_OUT_SIZE
                 CS_INTERFACE,
                 &[
                     MIDI_OUT_JACK_SUBTYPE,
                     EMBEDDED,
                     self.out_jack_id_emb(i), //id
-                    0x01, // 1 pin
-                    self.in_jack_id_ext(i), // pin is connected to this entity...
-                    0x01, // ...to the first pin
-                    0x00
-                ]
+                    0x01,                    // 1 pin
+                    self.in_jack_id_ext(i),  // pin is connected to this entity...
+                    0x01,                    // ...to the first pin
+                    0x00,
+                ],
             )?;
         }
 
         let mut endpoint_data = [
-            MS_GENERAL,
-            0, // number of jacks. must be filled in!
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 // jack mappings. must be filled in and cropped.
+            MS_GENERAL, 0, // number of jacks. must be filled in!
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, // jack mappings. must be filled in and cropped.
         ];
 
         writer.endpoint(&self.standard_bulkout)?; // len = 7
@@ -196,9 +212,10 @@ impl<B: UsbBus> UsbClass<B> for MidiClass<'_, B> {
         for i in 0..self.n_out_jacks {
             endpoint_data[2 + i as usize] = self.in_jack_id_emb(i);
         }
-        writer.write( // len = 4 + self.n_out_jacks
+        writer.write(
+            // len = 4 + self.n_out_jacks
             CS_ENDPOINT,
-            &endpoint_data[0..2+self.n_out_jacks as usize]
+            &endpoint_data[0..2 + self.n_out_jacks as usize],
         )?;
 
         writer.endpoint(&self.standard_bulkin)?; // len = 7
@@ -206,9 +223,10 @@ impl<B: UsbBus> UsbClass<B> for MidiClass<'_, B> {
         for i in 0..self.n_in_jacks {
             endpoint_data[2 + i as usize] = self.out_jack_id_emb(i);
         }
-        writer.write( // len = 4 + self.n_in_jacks
+        writer.write(
+            // len = 4 + self.n_in_jacks
             CS_ENDPOINT,
-            &endpoint_data[0..2+self.n_in_jacks as usize]
+            &endpoint_data[0..2 + self.n_in_jacks as usize],
         )?;
 
         let midi_streaming_end_byte = writer.position();
@@ -216,5 +234,4 @@ impl<B: UsbBus> UsbClass<B> for MidiClass<'_, B> {
 
         Ok(())
     }
-
 }
