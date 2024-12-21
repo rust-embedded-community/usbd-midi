a753386dcdeca943e711eee894be88e832b67a04
Oliver Rockstedt <info@sourcebox.de>
2024-12-21 13:15:32 +0100

    Re-export error types in crate root

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,10 @@ pub mod packet;
 #[cfg(feature = "message-types")]
 pub mod message;
 
-pub use crate::class::MidiClass;
+pub use crate::class::{MidiClass, MidiReadError};
 pub use crate::packet::cable_number::CableNumber;
 pub use crate::packet::reader::MidiPacketBufferReader;
-pub use crate::packet::UsbMidiEventPacket;
+pub use crate::packet::{MidiPacketParsingError, UsbMidiEventPacket};
 
 #[cfg(feature = "message-types")]
 pub use crate::message::Message;
